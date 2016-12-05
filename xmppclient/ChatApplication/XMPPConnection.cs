bb1d9c00b058c41556932b0ca264282dc14eb883
Wesley King <kingw92@gmail.com>
2016-12-04 22:51:47 -0500

    [WEEK6] - tweak xmppclient

--- xmppclient/ChatApplication/XMPPConnection.cs
+++ xmppclient/ChatApplication/XMPPConnection.cs
@@ -5,7 +5,6 @@ using S22.Xmpp;
 using S22.Xmpp.Client;
 using S22.Xmpp.Im;
 using System.Net.Security;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace ChatApplication
@@ -14,9 +13,14 @@ namespace ChatApplication
     {
         public XmppClient client;
         public XmppIm im;
-        string sendTo;
+        public string sendTo, receipientName,sentbyJID;
+        public TabPage userTabPage;
+
+        // True if the instance -im has been disposed of.
+        private bool disposed;
 
         delegate void SetConverstionLog(string text, string sentby, string sentto);
+        public delegate void SetTabCntrls(string receipientName);
 
         public XmppIm Im
         {
@@ -165,24 +169,26 @@ namespace ChatApplication
         {
             this.WindowState = FormWindowState.Maximized;
             InitializeComponent();
-            this.Load += new EventHandler(XMPPConnection_Load);
+            //this.Load += new EventHandler(XMPPConnection_Load);
         }
 
         private void XMPPConnection_Load(object sender, EventArgs e)
         {
-            XMPPConnect();
+            sendMsgtbx.Width = tableLayoutPanel1.Width;
+            Login lgn = new Login(this);
+            lgn.Show();
+            this.Hide();
         }
 
-        string username = "admin";
-        string password = "admin";
-
-        public void XMPPConnect()
+        public void XMPPConnect(string username,  string password)
         {
+            //this.Show();
             //string hostname = "jacurutu.net";
             //string username = "sindhu";
             //string password = "1234";
 
-            string hostname = "desktop-gm279k1";
+            string hostname = username.Split('@')[1];
+            username = username.Split('@')[0];
             //string username = "admin";
             //string password = "admin";
             int port = 5222; bool tls = true;
@@ -190,156 +196,200 @@ namespace ChatApplication
 
             im = new XmppIm(hostname, username, password, port, tls, validate);
             im.Message += OnNewMessage;
-            im.Connect();
-            populateUserlist();
+
+            try
+            {
+                im.Connect();
+            }
+            catch (XmppException e)
+            {
+                Console.Write(e);
+                return;
+            }
+            catch (ObjectDisposedException e)
+            {
+                Console.Write(e);
+                return;
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.Write(e);
+                return;
+            }
+            catch (System.Security.Authentication.AuthenticationException e)
+            {
+                Login lgn = new ChatApplication.Login(this);
+                lgn.setError();
+                lgn.Show();
+                Console.Write(e);
+                return;
+            }
+
+            this.Show();
+            lblUsername.Text = GetBareJid(im.Jid);
+
+            BindRoster();
         }
 
-        public void Authenticate(string username, string password)
+        private void BindRoster()
         {
-            im.Autenticate(username, password);
+            //Invoke XMPPRoster class to build Roster for the chat application.
+            XMPPRoster xmpproster = new XMPPRoster(this);
+            int location_ptr = 1;
+            panel4.Controls.Clear();
+
+            foreach (var item in xmpproster.rosteritems)
+            {
+                if (GetBareJid(item.Jid).Equals(im.Username)) {
+                    continue;
+                }
+                Button button = new Button();
+                panel4.Controls.Add(button);
+                button.Top = location_ptr * 50;
+                button.Image = Properties.Resources.usr_normal;
+                button.ImageAlign = ContentAlignment.MiddleLeft;
+                button.TextAlign = ContentAlignment.MiddleCenter;
+                button.Size = new Size(220, 50);
+                button.Margin = new Padding(0,0,0,0);
+                button.Text = GetBareJid(item.Jid);
+                button.Name = GetBareJid(item.Jid);
+                button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(236)))), ((int)(((byte)(252)))));
+                button.Click += new System.EventHandler(Button_Click);
+                button.ContextMenuStrip = contextMenuStrip;
+                location_ptr = location_ptr + 1;
+            }
+            //sendMsgtbx.Enabled = false;
         }
 
         private void refreshContactsbtn_Click(object sender, EventArgs e)
         {
-            populateUserlist();
+            BindRoster();
         }
 
-        private void populateUserlist()
-        {
-            int location_ptr = 1;
-            panel4.Controls.Clear();
-
-            foreach (var item in im.GetRoster())
-            {
-                Button button = new Button();
-                panel4.Controls.Add(button);
-                button.Top = location_ptr * 70;
-                button.Left = 13;
-                button.Size = new Size(150, 50);
-                button.Margin = new Padding(10, 10, 10, 10);
-                button.Text = item.Name;
-                button.Name = item.Jid.ToString();
-                button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(236)))), ((int)(((byte)(252)))));
-                button.Click += new System.EventHandler(this.Button_Click);
-                location_ptr = location_ptr + 1;
-            }
-
-            sendMsgtbx.Enabled = false;
-        }
-
-        private void Button_Click(object sender, EventArgs e)
+        protected void Button_Click(object sender, EventArgs e)
         {
             Button button = (sender as Button);
             sendMsgtbx.Enabled = true;
             sendMsgtbx.Focus();
             sendTo = button.Name.ToString();
+            receipientName = button.Text;
 
-            rosterbtnClick(sendTo);
+            startConversation(receipientName);
         }
 
-        private void sendMsgtbx_KeyDown(object sender,KeyEventArgs e)
+        private void startConversation(string receipientName)
         {
-           // MessageBox.Show(e.KeyCode.ToString());
+            //Using delegate to set the Tab controls from a different thread on a new oncoming message.
+            if (this.InvokeRequired)
+            {
+                SetTabCntrls delgateTabCntrl = new SetTabCntrls(startConversation);
+                object[] parameters = { receipientName };
+                this.Invoke(delgateTabCntrl, parameters);
+            }
+            else
+            {
+                //Invoke XMPPConverstion class to perform screen conversation updates
+                if (!tabControl.TabPages.ContainsKey(receipientName))
+                {
+                    userTabPage = new TabPage(receipientName);
+                    tabControl.TabPages.Add(userTabPage);
+                    convHistorytxb = new RichTextBox();
+                    convHistorytxb.Size = new Size(752, 654);
+                    convHistorytxb.AcceptsTab = true;
+                    convHistorytxb.ReadOnly = true;
+                    convHistorytxb.Dock = DockStyle.Fill;
+                    convHistorytxb.Location = new Point(3, 3);
+                    convHistorytxb.Name = receipientName;
+
+                    userTabPage.Controls.Add(convHistorytxb);
+                    userTabPage.Text = receipientName;
+                    userTabPage.Name = receipientName;
+
+                    tabControl.SelectedIndex = tabControl.TabPages.IndexOfKey(receipientName);
+                }
+                tabControl.SelectedIndex = tabControl.TabPages.IndexOfKey(receipientName);
+            }
+        }
+
+        private void sendMsgtbx_KeyUp(object sender,KeyEventArgs e)
+        {
             if (e.KeyCode == Keys.Enter)
             {
-                rosterbtnClick(sendTo);
+                //  startConversation(receipientName);
+                // string sentto = sendTo;
+                if (sendTo == null)
+                {
+                    sendTo = sentbyJID;
+                }
+                Jid tojid = new Jid(sendTo);
+                sendMessage(sendTo);
             }
         }
 
-        private void rosterbtnClick(string sendTo)
+        private void sendMessage(string sentto)
         {
-
-
-
-
-            if (!tabControl.TabPages.ContainsKey(sendTo))
+           string message = sendMsgtbx.Text;
+           if(!sentto.StartsWith(tabControl.SelectedTab.Text, StringComparison.OrdinalIgnoreCase))
             {
-                TabPage userTabPage = new TabPage(sendTo);
-                tabControl.TabPages.Add(userTabPage);
-                RichTextBox convHistorytxb = new RichTextBox();
-                convHistorytxb.Size = new System.Drawing.Size(752, 654);
-                convHistorytxb.AcceptsTab = true;
-                convHistorytxb.Dock = System.Windows.Forms.DockStyle.Fill;
-                convHistorytxb.Location = new System.Drawing.Point(3, 3);
-                //convHistorytxb.AutoSize = true;
-                convHistorytxb.Name = sendTo;
-
-                userTabPage.Controls.Add(convHistorytxb);
-                userTabPage.Text = sendTo;
-                userTabPage.Name = sendTo;
-
-                tabControl.SelectedIndex = tabControl.TabPages.IndexOfKey(sendTo);
-
+                string hostsuffix = sentto.Split('@')[1];
+                sentto = tabControl.SelectedTab.Text +'@' + hostsuffix;
             }
+
+            AssertValid();
+            im.SendMessage(sentto, message);
+            string sentby = GetBareJid(im.Jid);
+
+            UpdateConversationHistory(message, sentby, sentto);
+            sendMsgtbx.Text = string.Empty;
         }
 
         private void sndMsgbtn_Click(object sender, EventArgs e)
         {
             //Jid sendTo = "test@desktop-gm279k1";
             //Jid sendTo = "kingw";'
+
+            if(sendTo == null)
+            {
+                sendTo = sentbyJID;
+            }
             Jid tojid = new Jid(sendTo);
-            string message = sendMsgtbx.Text;
-            message.ThrowIfNull("message");
-            im.SendMessage(sendTo, message);
+            //string sentto = tojid.Node.ToString();
 
-            string sentby = im.Jid.Node.ToString();
-            string sentto = tojid.Node.ToString();
-            UpdateConversationHistory(message, sentby, sentto);
-            sendMsgtbx.Text = "";
-
+            sendMessage(sendTo);
         }
 
         //On receiving a new message from another client
         private void OnNewMessage(object sender, MessageEventArgs e)
         {
             string msg = e.Message.Body;
-            string sentby = e.Message.From.Node;
-            string sentto = e.Message.To.Node;
+            string sentby = GetBareJid(e.Message.From);
+            string sentto = GetBareJid(e.Message.To);
+            sentbyJID = GetBareJid(e.Message.From);
+
+            //sendMsgtbx.Enabled = true;
+            startConversation(sentby);
             UpdateConversationHistory(msg, sentby, sentto);
         }
 
         public void UpdateConversationHistory(string msg, string sentby, string sentto)
         {
-
             if (this.InvokeRequired)
             {
                 //Using delegate to update ConversationLog textbox from a different thread.
-                SetConverstionLog del = new SetConverstionLog(UpdateConversationHistory); //delegate
-                object[] parameters = { msg, sentby };
-                this.Invoke(del, parameters);
+                SetConverstionLog delgate = new SetConverstionLog(UpdateConversationHistory); //delegate
+                object[] parameters = { msg, sentby, sentto };
+                this.Invoke(delgate, parameters);
             }
 
             else
             {
                 string title = sentto;
-
-               
-
-                int tabIndex = tabControl.TabPages.IndexOfKey(tabControl.SelectedTab.Text);
-                
-               // MessageBox.Show(tabControl.SelectedTab.Text);
+                int tabIndex = tabControl.TabPages.IndexOf(tabControl.SelectedTab);
                 RichTextBox tmpbox = (RichTextBox)tabControl.TabPages[tabIndex].Controls[0];
                 tmpbox.Text += DateTime.Now + " " + sentby + ":\n";
                 tmpbox.Text += msg + "\n";
             }
         }
-
-        public void UpdateLog(object sender)
-        {
-
-        }
-
-        //public void UpdateContactlist(object sender, List<string> rosterlist)
-        //{
-        //    userList.Items.Clear();
-        //    foreach (string user in rosterlist)
-        //    {
-        //        if (user != client.Jid)
-        //            userList.Items.Add(user);
-        //    }
-        //}
-
-
 
         private void Searchtxb_leave(object sender, EventArgs e)
         {
@@ -371,11 +421,7 @@ namespace ChatApplication
             //    if (user.Contains(Searchtxb.Text))
             //        Nicknametxb = user.Name;
             //}
-
-
         }
-
-        
 
         private void ClearControls()
         {
@@ -387,13 +433,13 @@ namespace ChatApplication
         private void SearchContact(string s_contact)
         {
             ClearControls();
-            //lvContacts.Items.Clear();
-            if (s_contact == "")
+            if (s_contact == string.Empty)
             {
                 MessageBox.Show("Searchbtn Keyword is null");
             }
             else
             {
+                AssertValid();
                 foreach (var contact in im.GetRoster())
                 {
                     if (contact.Name.StartsWith(s_contact, StringComparison.OrdinalIgnoreCase))
@@ -409,9 +455,48 @@ namespace ChatApplication
             }
         }
 
-        private void XMPPConnection_Load_1(object sender, EventArgs e)
+        private void MyPopupEventHandler(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            if(tabControl.TabCount > 0)
+            {
+                newGrpConv.Enabled = true;
+            }
 
+        }
+
+        private void NewGroupConv_Click(object sender, EventArgs e)
+        {
+            string grpconvTitle = tabControl.SelectedTab.Text;
+            string addUsertogrpconv = ((ContextMenuStrip)(((ToolStripMenuItem)sender).Owner)).SourceControl.Text.ToString();
+            if(!grpconvTitle.Contains(addUsertogrpconv))
+            {
+                grpconvTitle += "," + addUsertogrpconv;
+                string Title = string.Empty;
+                    if(grpconvTitle.StartsWith("Group Chat", StringComparison.OrdinalIgnoreCase))
+                {
+                    Title += grpconvTitle;
+                }
+                    else
+                {
+                    Title = "Group Chat - " + grpconvTitle;
+                }
+                tabControl.SelectedTab.Text = Title;
+
+            }
+        }
+
+        public void AssertValid()
+        {
+            if (disposed)
+                throw new ObjectDisposedException(GetType().FullName);
+            if (!Connected)
+                throw new InvalidOperationException("Not connected to XMPP server.");
+            if (!Authenticated)
+                throw new InvalidOperationException("Not authenticated with XMPP server.");
+        }
+
+        private string GetBareJid(Jid jid) {
+            return jid.ToString().Split('@')[0];
         }
     }
 }
