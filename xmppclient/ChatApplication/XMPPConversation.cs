a49e4fb8965734248906dd2b2a3e1b4d46eb2627
Sindhu Balakrishnan <balas01@students.ipfw.edu>
2016-12-04 20:01:03 -0500

    xmppClient_v1

---
+++ xmppclient/ChatApplication/XMPPConversation.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace ChatApplication
+{
+    public partial class XMPPConversation : XMPPConnection
+    {
+        public XMPPConnection xmppconnection;
+
+        public XMPPConversation(XMPPConnection xmppconnection)
+        {
+            this.xmppconnection = xmppconnection;
+            
+        }
+    }
+}
